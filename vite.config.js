2112a8f93f520aac23cc2251a222e59f4603bf20
Akshit Tomar <akshitt125@gmail.com>
2023-08-14 00:02:28 +0530

    VITE CONFIG CHANGED

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,39 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
+
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env.VITE_SOME_KEY': JSON.stringify(process.env.VITE_SOME_KEY || '1234'),
+    
+//   },
+// });
+
+
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_SOME_KEY': JSON.stringify(process.env.VITE_SOME_KEY || '1234'),
-    
+  build: {
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+        globals: {
+          'import.meta.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
+          'import.meta.env.REACT_APP_API_KEY': JSON.stringify(process.env.REACT_APP_API_KEY),
+        },
+      },
+    },
   },
 });
