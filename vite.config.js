b80c67ea452e8a779ac2bbcce8bf56c673a19b98
Akshit Tomar <akshitt125@gmail.com>
2023-08-13 23:42:32 +0530

    VITE CONFIG CHANGED

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env.VITE_SOME_KEY': JSON.stringify(process.env.VITE_SOME_KEY || '1234'),
+    
+  },
+});
